a98932506e0b0a017cfac2ff512e6c807a36b9a7
Sidney Sousa <sidney.sousa@ifms.edu.br>
2020-10-13 12:44:38 -0400

    Primeira versão finalizada

--- index.js
+++ index.js
@@ -16,6 +16,41 @@ function alternarFormulario() {
 }
 
 /**
+ * Configurações do gráfico
+ */
+const opcoesGrafico = {
+    responsive: true,
+				title: {
+					display: true,
+					text: 'Dinheiro em caixa'
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				},
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
+				scales: {
+					xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Dias'
+						}
+					}],
+					yAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Renda'
+						}
+					}]
+				}
+};
+
+/**
  * Tratativa de lançamentos
  */
 
@@ -23,12 +58,16 @@ let lancamentosArmazenados = localStorage.getItem('lancamentos');
 let lancamentos =
     lancamentosArmazenados ? JSON.parse(lancamentosArmazenados) : [];
 renderizarLancamentos();
+renderizarGrafico();
 
 function lancar(event) {
     event.preventDefault();
 
+
+    const multiplicadorLancamento = $('#gasto').checked ? -1 : 1;
+
     let lancamento = {
-        valor: parseFloat($('#valor').value),
+        valor: parseFloat($('#valor').value) * multiplicadorLancamento,
         descricao: $('#descricao').value,
         dataLancamento: $('#dataLancamento').value,
     };
@@ -37,7 +76,47 @@ function lancar(event) {
     armazenarLancamentos();
     limparFormulario();
     renderizarLancamentos();
+    renderizarGrafico();
     $('#valor').focus();
+}
+
+function renderizarGrafico() {
+    if(lancamentos) {
+        const lancamentosOrdenados = 
+            lancamentos.sort((a, b) => a.dataLancamento - b.dataLancamento);
+        
+            let datas = [];
+            let valores = [];
+            let valorAtual = 0;
+            lancamentosOrdenados.forEach(lancamento => {
+                const data =
+                    new Date(lancamento.dataLancamento).toLocaleDateString();
+                datas.push(data);
+
+                valorAtual += lancamento.valor;
+                valores.push(valorAtual);
+            });
+
+
+            const corCurva = valorAtual < 0 ? 'red' : 'blue';
+            const config = {
+                type: 'line',
+                data: {
+                    labels: datas,
+                    datasets: [{
+                        label: 'Comportamento do seu dinheiro',
+                        backgroundColor: corCurva,
+                        borderColor: corCurva,
+                        data: valores,
+                        fill: false
+                    }]
+                },
+                options: opcoesGrafico
+            };
+
+            const contexto = $('#graficoLancamentos').getContext('2d');
+            new Chart(contexto, config);
+    }
 }
 
 function armazenarLancamentos() {
